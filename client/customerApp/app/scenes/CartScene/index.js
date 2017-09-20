b62456379dff64ba9104fb83d2ba9210e45445ab
Patrick Puritscher <hello@patrickpuritscher.com>
2017-09-20 23:00:07 +0300

    restore...

--- client/customerApp/app/scenes/CartScene/index.js
+++ client/customerApp/app/scenes/CartScene/index.js
@@ -3,20 +3,20 @@ import { Text } from 'react-native';
 import { StackNavigator } from 'react-navigation';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 
-import OrdersOverviewScreen from '../../screens/OrdersOverviewScreen';
+import CartOverviewScreen from '../../screens/CartOverviewScreen';
 
 const ModalStack = StackNavigator({
-  Orders: {
-    screen: OrdersOverviewScreen,
+  Cart: {
+    screen: CartOverviewScreen,
   },
 });
 
 export default class extends Component {
   static navigationOptions = {
-    tabBarLabel: 'Orders',
+    tabBarLabel: 'Cart',
     // Note: By default the icon is only shown on iOS. Search the showIcon option below.
     tabBarIcon: ({ tintColor }) => (
-      <Icon name="shopping-cart" size={27} color={tintColor} />
+      <Icon name="list" size={27} color={tintColor} />
     ),
   };
   render() {
