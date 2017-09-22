1dddd3430949b9428340e0d17cce99a5c71cac80
Patrick Puritscher <hello@patrickpuritscher.com>
2017-09-22 15:52:22 +0300

    added productCell

---
+++ client/customerApp/app/components/IconButton/index.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+
+export default ({ iconName, label }) => (
+  <TouchableOpacity>
+    <View style={styles.container}>
+      <Icon name={iconName} size={20} color="#fff" />
+      <Text style={styles.label}>{label}</Text>
+    </View>
+  </TouchableOpacity>
+);
+
+const styles = StyleSheet.create({
+  container: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: 'rgb(230,37,101)',
+    paddingVertical: 5,
+    borderRadius: 5,
+  },
+  label: {
+    color: '#fff',
+    fontWeight: 'bold',
+    marginTop: 5,
+    fontSize: 13,
+  },
+});
--- client/customerApp/app/container/BrowseCategoryContainer/index.js
+++ client/customerApp/app/container/BrowseCategoryContainer/index.js
@@ -1,8 +1,55 @@
 import React, { PureComponent } from 'react';
 import { Text } from 'react-native';
 
+import { TableView, Section } from 'react-native-tableview-simple';
+import ProductCell from '../../components/ProductCell';
+
 export default class extends PureComponent {
   render() {
-    return <Text>Milk!</Text>;
+    return (
+      <TableView>
+        <Section
+          sectionTintColor="#E9E9EF"
+          footer="I accept that the prices may vary. Also I accept, that there's no guarantee that the products are available at the written stores."
+        >
+          <ProductCell
+            price="1.99 €"
+            title="Low Fat Milk"
+            subtitle="0.5 l (1 l = 3.98 €)"
+            stores={['Lidl']}
+            imageSource={{
+              uri: 'https://facebook.github.io/react/img/logo_og.png',
+            }}
+          />
+          <ProductCell
+            price="1.99 €"
+            title="Lactose free"
+            subtitle="0.5 l (1 l = 3.98 €)"
+            stores={['Alepa', 'K-Market']}
+            imageSource={{
+              uri: 'https://facebook.github.io/react/img/logo_og.png',
+            }}
+          />
+          <ProductCell
+            price="1.99 €"
+            title="Piima"
+            subtitle="0.5 l (1 l = 3.98 €)"
+            stores={['Lidl']}
+            imageSource={{
+              uri: 'https://facebook.github.io/react/img/logo_og.png',
+            }}
+          />
+          <ProductCell
+            price="1.99 €"
+            title="Regular"
+            subtitle="0.5 l (1 l = 3.98 €)"
+            stores={['S-Market']}
+            imageSource={{
+              uri: 'https://facebook.github.io/react/img/logo_og.png',
+            }}
+          />
+        </Section>
+      </TableView>
+    );
   }
 }
